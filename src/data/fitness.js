6046fbee9e8d8adbcc586d7bfb244dd0069484d3
msoni110 <msoni@beesolvertechnology.in>
2025-07-02 17:43:56 +0530

    Add fitness exercise data and update .gitignore
    Introduced a new src/data/fitness.js file containing structured data for various muscle groups and their exercises, including images and set information. Also updated .gitignore to stop ignoring the src/data directory.

---
+++ src/data/fitness.js
@@ -0,0 +1,352 @@
+export default exerciseList = [
+  {
+    id: "3f8b3dc4-f344-4b4f-a0c2-6d9d1e92e5d5",
+    name: "Biceps",
+    image:
+      "https://intowellness.in/wp-content/uploads/2024/10/Into_Wellness_Biceps_Curl.jpg",
+    exercises: [
+      {
+        id: "b1",
+        name: "Biceps Curl",
+        image:
+          "https://intowellness.in/wp-content/uploads/2024/10/Into_Wellness_Biceps_Curl.jpg",
+        sets: "10 X 3",
+      },
+      {
+        id: "b2",
+        name: "Hammer Curl",
+        image:
+          "https://intowellness.in/wp-content/uploads/2024/10/Into_Wellness_Hammer_Curl.jpg",
+        sets: "12 X 3",
+      },
+      {
+        id: "b3",
+        name: "Concentration Curl",
+        image:
+          "https://intowellness.in/wp-content/uploads/2024/10/Into_Wellness_Concentration_Curl.jpg",
+        sets: "10 X 3",
+      },
+      {
+        id: "b4",
+        name: "Cable Curl",
+        image:
+          "https://intowellness.in/wp-content/uploads/2024/10/Into_Wellness_Cable_Curl.jpg",
+        sets: "12 X 3",
+      },
+      {
+        id: "b5",
+        name: "Barbell Curl",
+        image:
+          "https://intowellness.in/wp-content/uploads/2024/10/Into_Wellness_Barbell_Curl.jpg",
+        sets: "10 X 4",
+      },
+      {
+        id: "b6",
+        name: "Zottman Curl",
+        image:
+          "https://intowellness.in/wp-content/uploads/2024/10/Into_Wellness_Zottman_Curl.jpg",
+        sets: "12 X 3",
+      },
+    ],
+  },
+  {
+    id: "02c7bd34-cd94-4c50-b0cf-92f4c1d6f7f0",
+    name: "Chest",
+    image:
+      "https://intowellness.in/wp-content/uploads/2024/10/Into_Wellness_Chest_Press.jpg",
+    exercises: [
+      {
+        id: "c1",
+        name: "Chest Press",
+        image:
+          "https://intowellness.in/wp-content/uploads/2024/10/Into_Wellness_Chest_Press.jpg",
+        sets: "10 X 4",
+      },
+      {
+        id: "c2",
+        name: "Incline Bench Press",
+        image:
+          "https://intowellness.in/wp-content/uploads/2024/10/Into_Wellness_Incline_Press.jpg",
+        sets: "8 X 4",
+      },
+      {
+        id: "c3",
+        name: "Decline Bench Press",
+        image:
+          "https://intowellness.in/wp-content/uploads/2024/10/Into_Wellness_Decline_Bench_Press.jpg",
+        sets: "8 X 4",
+      },
+      {
+        id: "c4",
+        name: "Dumbbell Flyes",
+        image:
+          "https://intowellness.in/wp-content/uploads/2024/10/Into_Wellness_Dumbbell_Flyes.jpg",
+        sets: "12 X 3",
+      },
+      {
+        id: "c5",
+        name: "Pec Deck Machine",
+        image:
+          "https://intowellness.in/wp-content/uploads/2024/10/Into_Wellness_Pec_Deck.jpg",
+        sets: "12 X 3",
+      },
+      {
+        id: "c6",
+        name: "Push-ups",
+        image:
+          "https://intowellness.in/wp-content/uploads/2024/10/Into_Wellness_Push_Ups.jpg",
+        sets: "15 X 3",
+      },
+    ],
+  },
+  {
+    id: "dc2853b3-f1cf-4df3-97ea-2e9306e57d1a",
+    name: "Triceps",
+    image:
+      "https://intowellness.in/wp-content/uploads/2024/10/Into_Wellness_Triceps_Extension.jpg",
+    exercises: [
+      {
+        id: "t1",
+        name: "Triceps Extension",
+        image:
+          "https://intowellness.in/wp-content/uploads/2024/10/Into_Wellness_Triceps_Extension.jpg",
+        sets: "12 X 3",
+      },
+      {
+        id: "t2",
+        name: "Triceps Pushdown",
+        image:
+          "https://intowellness.in/wp-content/uploads/2024/10/Into_Wellness_Triceps_Pushdown.jpg",
+        sets: "10 X 3",
+      },
+      {
+        id: "t3",
+        name: "Overhead Dumbbell Extension",
+        image:
+          "https://intowellness.in/wp-content/uploads/2024/10/Into_Wellness_Overhead_Extension.jpg",
+        sets: "12 X 3",
+      },
+      {
+        id: "t4",
+        name: "Skull Crushers",
+        image:
+          "https://intowellness.in/wp-content/uploads/2024/10/Into_Wellness_Skull_Crushers.jpg",
+        sets: "10 X 3",
+      },
+      {
+        id: "t5",
+        name: "Close-Grip Bench Press",
+        image:
+          "https://intowellness.in/wp-content/uploads/2024/10/Into_Wellness_Close_Grip_Press.jpg",
+        sets: "10 X 4",
+      },
+      {
+        id: "t6",
+        name: "Dips",
+        image:
+          "https://intowellness.in/wp-content/uploads/2024/10/Into_Wellness_Dips.jpg",
+        sets: "12 X 3",
+      },
+    ],
+  },
+  {
+    id: "b3ff44ef-e2c9-4ea3-b1c9-0e62ac3b44e2",
+    name: "Shoulders",
+    image:
+      "https://intowellness.in/wp-content/uploads/2024/10/Into_Wellness_Shoulder_Press.jpg",
+    exercises: [
+      {
+        id: "s1",
+        name: "Shoulder Press",
+        image:
+          "https://intowellness.in/wp-content/uploads/2024/10/Into_Wellness_Shoulder_Press.jpg",
+        sets: "8 X 3",
+      },
+      {
+        id: "s2",
+        name: "Lateral Raise",
+        image:
+          "https://intowellness.in/wp-content/uploads/2024/10/Into_Wellness_Lateral_Raise.jpg",
+        sets: "12 X 3",
+      },
+      {
+        id: "s3",
+        name: "Front Raise",
+        image:
+          "https://intowellness.in/wp-content/uploads/2024/10/Into_Wellness_Front_Raise.jpg",
+        sets: "10 X 3",
+      },
+      {
+        id: "s4",
+        name: "Reverse Flyes",
+        image:
+          "https://intowellness.in/wp-content/uploads/2024/10/Into_Wellness_Reverse_Flyes.jpg",
+        sets: "12 X 3",
+      },
+      {
+        id: "s5",
+        name: "Arnold Press",
+        image:
+          "https://intowellness.in/wp-content/uploads/2024/10/Into_Wellness_Arnold_Press.jpg",
+        sets: "10 X 3",
+      },
+      {
+        id: "s6",
+        name: "Upright Row",
+        image:
+          "https://intowellness.in/wp-content/uploads/2024/10/Into_Wellness_Upright_Row.jpg",
+        sets: "10 X 3",
+      },
+    ],
+  },
+  {
+    id: "178ecbc9-25ce-4c29-84d3-d8bfc4f3f498",
+    name: "Legs",
+    image:
+      "https://intowellness.in/wp-content/uploads/2024/10/Into_Wellness_Leg_Press.jpg",
+    exercises: [
+      {
+        id: "l1",
+        name: "Leg Press",
+        image:
+          "https://intowellness.in/wp-content/uploads/2024/10/Into_Wellness_Leg_Press.jpg",
+        sets: "12 X 4",
+      },
+      {
+        id: "l2",
+        name: "Squats",
+        image:
+          "https://intowellness.in/wp-content/uploads/2024/10/Into_Wellness_Squat.jpg",
+        sets: "10 X 4",
+      },
+      {
+        id: "l3",
+        name: "Lunges",
+        image:
+          "https://intowellness.in/wp-content/uploads/2024/10/Into_Wellness_Lunges.jpg",
+        sets: "12 X 3",
+      },
+      {
+        id: "l4",
+        name: "Leg Extension",
+        image:
+          "https://intowellness.in/wp-content/uploads/2024/10/Into_Wellness_Leg_Extension.jpg",
+        sets: "12 X 3",
+      },
+      {
+        id: "l5",
+        name: "Hamstring Curl",
+        image:
+          "https://intowellness.in/wp-content/uploads/2024/10/Into_Wellness_Hamstring_Curl.jpg",
+        sets: "12 X 3",
+      },
+      {
+        id: "l6",
+        name: "Calf Raise",
+        image:
+          "https://intowellness.in/wp-content/uploads/2024/10/Into_Wellness_Calf_Raise.jpg",
+        sets: "15 X 3",
+      },
+    ],
+  },
+  {
+    id: "7ddfc88a-0ad4-4f4a-8888-6d81eb3e702f",
+    name: "Back",
+    image:
+      "https://intowellness.in/wp-content/uploads/2024/10/Into_Wellness_Lat_Pulldown.jpg",
+    exercises: [
+      {
+        id: "bk1",
+        name: "Lat Pulldown",
+        image:
+          "https://intowellness.in/wp-content/uploads/2024/10/Into_Wellness_Lat_Pulldown.jpg",
+        sets: "10 X 3",
+      },
+      {
+        id: "bk2",
+        name: "Deadlift",
+        image:
+          "https://intowellness.in/wp-content/uploads/2024/10/Into_Wellness_Deadlift.jpg",
+        sets: "6 X 3",
+      },
+      {
+        id: "bk3",
+        name: "Bent-over Row",
+        image:
+          "https://intowellness.in/wp-content/uploads/2024/10/Into_Wellness_Bentover_Row.jpg",
+        sets: "10 X 3",
+      },
+      {
+        id: "bk4",
+        name: "Seated Cable Row",
+        image:
+          "https://intowellness.in/wp-content/uploads/2024/10/Into_Wellness_Seated_Row.jpg",
+        sets: "10 X 3",
+      },
+      {
+        id: "bk5",
+        name: "T-Bar Row",
+        image:
+          "https://intowellness.in/wp-content/uploads/2024/10/Into_Wellness_Tbar_Row.jpg",
+        sets: "8 X 3",
+      },
+      {
+        id: "bk6",
+        name: "Pull-ups",
+        image:
+          "https://intowellness.in/wp-content/uploads/2024/10/Into_Wellness_Pullups.jpg",
+        sets: "Max X 3",
+      },
+    ],
+  },
+  {
+    id: "bfea4f30-6f6b-4e0e-a1f0-92511e21ab32",
+    name: "Abs",
+    image:
+      "https://intowellness.in/wp-content/uploads/2024/10/Into_Wellness_Ab_Crunch.jpg",
+    exercises: [
+      {
+        id: "a1",
+        name: "Ab Crunch",
+        image:
+          "https://intowellness.in/wp-content/uploads/2024/10/Into_Wellness_Ab_Crunch.jpg",
+        sets: "15 X 3",
+      },
+      {
+        id: "a2",
+        name: "Leg Raises",
+        image:
+          "https://intowellness.in/wp-content/uploads/2024/10/Into_Wellness_Leg_Raises.jpg",
+        sets: "15 X 3",
+      },
+      {
+        id: "a3",
+        name: "Russian Twists",
+        image:
+          "https://intowellness.in/wp-content/uploads/2024/10/Into_Wellness_Russian_Twist.jpg",
+        sets: "20 X 3",
+      },
+      {
+        id: "a4",
+        name: "Plank",
+        image:
+          "https://intowellness.in/wp-content/uploads/2024/10/Into_Wellness_Plank.jpg",
+        sets: "60s X 3",
+      },
+      {
+        id: "a5",
+        name: "Bicycle Crunches",
+        image:
+          "https://intowellness.in/wp-content/uploads/2024/10/Into_Wellness_Bicycle_Crunch.jpg",
+        sets: "20 X 3",
+      },
+      {
+        id: "a6",
+        name: "Cable Crunch",
+        image:
+          "https://intowellness.in/wp-content/uploads/2024/10/Into_Wellness_Cable_Crunch.jpg",
+        sets: "15 X 3",
+      },
+    ],
+  },
+];
